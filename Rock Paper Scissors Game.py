c95ab7cece98efc5b0db29913fede62325bb2d70
Sleepyy9 <mightybj10@gmail.com>
2025-05-07 22:30:21 +0100

    Update Rock Paper Scissors Game.py
    My own style.

--- Rock Paper Scissors Game.py
+++ Rock Paper Scissors Game.py
@@ -1,34 +1,45 @@
 import random
 
-options = ("rock", "paper", "scissors")
-running = True
 
-while running:
+def game():
+    options = ("rock", "paper", "scissors")
+    flag = True
 
-    player = None 
-    computer = random.choice(options) 
+    while flag:
 
-    while player not in options:
-        player = input("Enter a choice (rock, paper, scissors): ")
+        player = None 
+        computer = random.choice(options) 
 
-    print(f"Player: {player}")
-    print(f"Computer: {computer}")
+        while player not in options:
+            player = input("Enter a choice (rock, paper, scissors)🪨 📃 ✂️ : ")
 
-    if player == computer:
-        print("It's a tie!")
-    elif player == "rock" and computer == "scissors":
-        print("You win!")
-    elif player == "paper" and computer == "rock":
-        print("You win!")
-    elif player == "scissors" and computer == "paper":
-        print("You win!")
-    else:
-        print("You lose!")
+        print("")
+        print(f"👶 You: {player}")
+        print(f"🖥️ Computer: {computer}")
 
-#   if input("Play again? (y/n): ).lower() == "y": -- Faster way of doing it
-    play_again = input("Play again? (y/n): ").lower()
-    if not play_again == "y":
-        running = False  
+        if player == computer:
+            print("you TIED! 😭")
+        elif player == "rock" and computer == "scissors":
+            print("You win!!🥳")
+        elif player == "paper" and computer == "rock":
+            print("You win!!🥳")
+        elif player == "scissors" and computer == "paper":
+            print("You win!!🥳")
+        else:
+            print("Vro you some booty cheeks 💩 How you loosing to a bot😭 ✌️ ☠️ 🥀")
+
+    #   if input("Play again? (y/n): ).lower() == "y": -- Faster way of doing it   
+        play_again = input("Wanna play again? (y/n): ").lower()
+        if not play_again == "y":
+            flag = False  
+
+    print("####################################################################")
+    print("###################### Thanks for playing 💖 ######################")
+    print("####################################################################")
 
 
-print("Thanks for playing")
+print("#### ROCK 🪨 #####################################")
+print("############### PAPER 📃 #########################")
+print("########################## SCISSORS ✂️############")
+
+game("")
